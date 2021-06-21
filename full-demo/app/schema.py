9575d042be4c46e2d218653b885726d1c60ed673
LordGhostX <solomonghost9@gmail.com>
2021-06-21 04:46:17 +0100

    added allNotes query

--- full-demo/app/schema.py
+++ full-demo/app/schema.py
@@ -15,14 +15,13 @@ class createNote(graphene.Mutation):
     class Arguments:
         title = graphene.String()
         body = graphene.String()
-    ok = graphene.Boolean()
     note = graphene.Field(lambda: Note)
 
     def mutate(self, info, title, body):
         note = Notes(title=title, body=body)
         db.session.add(note)
         db.session.commit()
-        return createNote(note=note, ok=True)
+        return createNote(note=note)
 
 
 class Mutation(graphene.ObjectType):
@@ -30,17 +29,14 @@ class Mutation(graphene.ObjectType):
 
 
 class Query(graphene.ObjectType):
+    all_notes = graphene.List(Note)
     get_note = graphene.Field(Note, id=graphene.String(required=True))
 
+    def resolve_all_notes(self, info):
+        return Notes.query.all()
+
     def resolve_get_note(self, info, id):
-        note = Notes.query.get(id)
-        return Note(
-            id=note.id,
-            title=note.title,
-            body=note.body,
-            last_updated=note.last_updated,
-            date_created=note.date_created
-        )
+        return Notes.query.get(id)
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
